67fa731bcf8d490c94ad25c5d6fcf8066536e5a4
EJ <elliot.jones103@gmail.com>
2025-11-18 21:27:17 +0000

    bubble detector

---
+++ bubble_detector/data.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+import yfinance as yf
+for datetime import datetime, timezone
+
+def fetch_yahoo_option_chain(ticker = "", expiry = ""):
+    
+    tkr = yf.ticker(ticker)
+    hist = tkr.history(period="1d")
+    if hist.empty:
+        raise ValueError(f"Ticker {ticker} not found")
+    S0 = hist["Close"].iloc[-1]
+
+    # expiry date
+    expires = tkr.options
+    if not expires:
+        raise ValueError(f"No options found for ticker {ticker}")
+    if expiry is None:
+        expiry = expires[0]
+    elif expiry not in expires:
+        raise ValueError(f"Expiry {expiry} not found for ticker {ticker}")
+    
+    chain = tkr.option_chain(ex)
